3de6fef7899169820e9ff60a88e521b27c0cb52e
rahimliparvin <parvinvr@code.edu.az>
2023-06-27 20:02:15 +0400

    Start backend front to back

---
+++ Savoy/Savoy/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Savoy.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+
+            if (id == null) return BadRequest();
+
+            //Product product = await _productService.GetFullDataByIdAsync(id);
+
+            //if (product == null) return NotFound();
+
+            return View();
+
+        }
+    }
+}
---
+++ Savoy/Savoy/Controllers/ShopController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Savoy.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> Detail(int id)
+        {
+
+            return View();
+        }
+    }
+}
